5cd77c77531e37ce303ab703d6d0b8b07755ff7f
jimmymetts <jimmymetts@bellsouth.net>
2019-12-16 16:42:33 -0600

    added json and animalList

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from "react-router-dom"
+import Kennel from './components/Kennel'
 
-import Kennel from './components/Kennel';
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Kennel />
+  </Router>
+  , document.getElementById('root'))
 
 
