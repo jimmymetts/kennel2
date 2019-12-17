70d27a8a19069eef4dbf60ffde3538139c9e1d05
jimmymetts <jimmymetts@bellsouth.net>
2019-12-17 06:46:04 -0600

    added animal css

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import './Animal.css'
+import '../Kennel.css'
+
 
 class AnimalCard extends Component {
   render() {
     return (
       <div className="card">
         <div className="card-content">
-          <picture>
+          <picture >
             <img src={require('./dog.svg')} alt="My Dog" />
           </picture>
           <h3>Name: <span className="card-petname">Doodles</span></h3>
