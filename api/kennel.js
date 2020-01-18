ff65f1ca81ef43ab46da20c4a2c458f9df223600
jimmymetts <jimmymetts@bellsouth.net>
2020-01-18 17:32:18 -0600

    added Theo to pet list

--- api/kennel.js
+++ api/kennel.js
@@ -3,7 +3,8 @@
         { "id": 1, "name": "Doodles", "breed": "Poodle"},
         { "id": 2, "name": "Double Decker", "breed": "German Sheperd" },
         { "id": 3, "name": "Esme", "breed": "Pitbull" },
-        { "id": 3, "name": "Jake", "breed": "Blue Heeler" }
+        { "id": 4, "name": "Jake", "breed": "Blue Heeler" },
+        { "id": 5, "name": "Theo", "breed": "Choc Lab" }
 
     ]
 }
