13c09f6899edff12f54481ccbe65c63e2980881e
jimmymetts <jimmymetts@bellsouth.net>
2020-01-18 17:21:44 -0600

    changed location

--- api/kennel.js
+++ api/kennel.js
@@ -2,6 +2,8 @@
     "animals": [
         { "id": 1, "name": "Doodles", "breed": "Poodle"},
         { "id": 2, "name": "Double Decker", "breed": "German Sheperd" },
-        { "id": 3, "name": "Esme", "breed": "Pitbull" }
+        { "id": 3, "name": "Esme", "breed": "Pitbull" },
+        { "id": 3, "name": "Jake", "breed": "Blue Heeler" }
+
     ]
 }
