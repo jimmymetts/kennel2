8d8cfd8b71c3ba11f0745ba02f26495e129a3cb1
jimmymetts <jimmymetts@bellsouth.net>
2020-01-19 09:13:43 -0600

    add Pebbles

--- api/kennel.js
+++ api/kennel.js
@@ -4,7 +4,8 @@
         { "id": 2, "name": "Double Decker", "breed": "German Sheperd" },
         { "id": 3, "name": "Esme", "breed": "Pitbull" },
         { "id": 4, "name": "Jake", "breed": "Blue Heeler" },
-        { "id": 5, "name": "Theo", "breed": "Choc Lab" }
+        { "id": 5, "name": "Theo", "breed": "Choc Lab" },
+        { "id": 5, "name": "Pebbles", "breed": "Golden Retriever" }
 
     ]
 }
