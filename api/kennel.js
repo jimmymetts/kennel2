f930b10666a2989e6d4d56c46d722fb8c53d464b
jimmymetts <jimmymetts@bellsouth.net>
2020-01-19 19:50:18 -0600

    add Buddy

--- api/kennel.js
+++ api/kennel.js
@@ -5,8 +5,8 @@
         { "id": 3, "name": "Esme", "breed": "Pitbull" },
         { "id": 4, "name": "Jake", "breed": "Blue Heeler" },
         { "id": 5, "name": "Theo", "breed": "Choc Lab" },
-        { "id": 6, "name": "Pebbles", "breed": "Golden Retriever" }
-        { "id": 5, "name": "Pebbles", "breed": "Golden Retriever" }
+        { "id": 6, "name": "Pebbles", "breed": "Golden Retriever" },
+        { "id": 7, "name": "Buddy", "breed": "Fiest" }
 
     ]
 }
